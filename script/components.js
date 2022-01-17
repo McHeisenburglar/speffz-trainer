8326801fdff0e1a53aa8ee45cbd25b24081b2c68
Irakli Goderdzishvili <irakli.goder@gmail.com>
2022-01-17 18:46:45 +0400

    Add corner mode and tab bar.

--- script/components.js
+++ script/components.js
@@ -23,3 +23,25 @@ export function edgePiece(sticker) {
 	`;
 	return element;
 }
+
+export function cornerPiece(sticker) {
+	const element = document.createElement('div');
+	element.classList.add('piece-parent');
+	element.innerHTML = `
+	<svg
+		class="piece corner ${sticker}"
+		viewBox="0 0 200 200"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			class="sticker sticker-primary"
+			d="M99.9031 11L179.806 66.9031L99.9031 122.806L20 66.9031L99.9031 11Z"
+			fill="white"
+		/>
+		<path class="sticker sticker-secondary" d="M29 134L100 190V123L20 67L29 134Z" fill="#0797FF" />
+		<path class="sticker sticker-tertiary" d="M171 134L100 190V123L180 67L171 134Z" fill="#B822FF" />
+	</svg>
+		`;
+	return element;
+}
